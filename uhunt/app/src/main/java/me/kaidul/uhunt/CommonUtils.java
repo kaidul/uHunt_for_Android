bbbc7c6b3cf86db84ae3af37eda704feb22b2b0a
esraa-ibrahim <esraacs2012@gmail.com>
2019-05-03 16:40:34 +0200

    Add gradle build system, remove pull to refresh, actionbarsherlock, and sherlockprogressfragment libraries

---
+++ uhunt/app/src/main/java/me/kaidul/uhunt/CommonUtils.java
@@ -0,0 +1,96 @@
+/*
+ * uHunt for Android - The most comprehensive Android app for uHunt and Competitive programming
+ * Copyright (C) 2013 Kaidul Islam
+ * 
+ * This file is part of uHunt for Android.
+
+ * uHunt for Android is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * uHunt for Android is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with uHunt for Android.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package me.kaidul.uhunt;
+
+public class CommonUtils {
+
+	final static boolean isDebuggable = false;
+	
+	final static String API_URL_PREFIX = "https://uhunt.onlinejudge.org/api/";
+	final static String USER_SUBMISSION_URL = API_URL_PREFIX + "subs-user-last/";
+	final static String LIVE_SUBMISSION_URL = API_URL_PREFIX + "poll/";
+	final static String RANKLIST_URL = API_URL_PREFIX + "ranklist/";
+	final static String ALL_SUBMISSIONS = API_URL_PREFIX + "subs-user/";
+	final static String USER_ID_TO_USERNAME = API_URL_PREFIX + "uname2uid/";
+	final static String SPECIFIC_PROBLEM_URL = API_URL_PREFIX + "p/num/";
+	final static String PREFERENCE_NAME = "me.kaidul.uhunt";
+	final static String PROBLEM_LIST_URL = API_URL_PREFIX + "p";
+	final static String FILE_ALL_SUBMISSIONS = "all_submissions.json";
+	final static String FILE_PROBLEM_LIST = "problem_list.json";
+	final static String FILE_COMPETITIVE_PROGRAMMING_3 = "competitive_programming_edition_3.json";
+	final static String PROBLEM_URL = "http://uva.onlinejudge.org/external/";
+	final static String KEY_USERNAME = "uname";
+	final static String KEY_NAME = "name";
+	final static String KEY_SUBMISSION = "subs";
+	final static String KEY_ID = "id";
+	final static String KEY_SUBMISSION_ID = "sid";
+	final static String KEY_PROBLEM_ID = "pid";
+	final static String KEY_USER_ID = "uid";
+	final static String KEY_VERDICT_ID = "ver";
+	final static String KEY_VERDICT_COLOR = "verdict_color";
+	final static String KEY_RUNTIME = "run";
+	final static String KEY_SUBMISSION_TIME = "sbt";
+	final static String KEY_LANGUAGE_ID = "lan";
+	final static String KEY_SUBMISSION_RANK = "rank";
+	final static String KEY_MESSAGE = "msg";
+
+	final static String LAST_SAVED = "last_saved";
+	final static String KEY_RANK_USERNAME = "username";
+	final static String KEY_RANK_AC = "ac";
+	final static String KEY_RANK_NOS = "nos";
+	
+	final static String PROBLEM_DATABASE = "problem_database.db";
+	final static int PROBLEM_DATABASE_VERSION = 1;
+	final static String PROBLEM_TABLE = "problem_table";
+	final static String KEY_PROBLEM_TITLE = "title";
+	final static String KEY_PROBLEM_NO = "num";
+	final static String KEY_PROBLEM_NO_TITLE = "no_and_title";
+	final static String KEY_VERDICT_SERIES = "verdict_series";
+	final static String KEY_DACU = "dacu";
+	final static String KEY_BEST_RT = "mrun";
+	final static String KEY_BEST_M = "mmem";
+	final static String KEY_NO_VERDICT = "nover";
+	final static String KEY_NO_SE = "sube";
+	final static String KEY_NO_NOT_JUDGE = "noj";
+	final static String KEY_NO_IN_QUEUE = "inq";
+	final static String KEY_NO_CE = "ce";
+	final static String KEY_NO_RF = "rf";
+	final static String KEY_NO_RE = "re";
+	final static String KEY_NO_OLE = "ole";
+	final static String KEY_NO_TLE = "tle";
+	final static String KEY_NO_MLE = "mle";
+	final static String KEY_NO_WA = "wa";
+	final static String KEY_NO_PE = "pe";
+	final static String KEY_NO_AC = "ac";
+	final static String KEY_NO_RTL = "rtl";
+	final static String KEY_NO_STATUS = "status";
+	final static String[] alias = {"90", "80", "70", "50", "60", "30", "40", "10"};
+	final static String[] aliases = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"};
+	final static String KEY_BOOK_NAME = "competitive_programming_edition_3.txt";
+	
+	final static String ALL_VERDICT_TOGETHER = "all_verdicts_together";
+	final static String submissionIsCached = "submissionIsCached";
+	final static String problemListisCached = "problem_is_cached";
+	
+	final static String DEFAULT_USER_ID = "135364";
+	final static String DEFAULT_USERNAME = "Kaidul";
+	final static String ABOUT_HTML = "file:///android_asset/about.html";
+	
+}
---
+++ uhunt/app/src/main/java/me/kaidul/uhunt/LiveSubmissions.java
@@ -0,0 +1,337 @@
+/*
+ * uHunt for Android - The most comprehensive Android app for uHunt and Competitive programming
+ * Copyright (C) 2013 Kaidul Islam
+ * 
+ * This file is part of uHunt for Android.
+
+ * uHunt for Android is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * uHunt for Android is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with uHunt for Android.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package me.kaidul.uhunt;
+
+import android.annotation.SuppressLint;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+
+import com.devspark.progressfragment.ProgressFragment;
+import com.google.gson.stream.JsonReader;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+
+public class LiveSubmissions extends ProgressFragment {
+
+	private View mContentView;
+	ListView list;
+	LiveSubmissionAdapter adapter = null;
+	private String sinceID;
+	ArrayList<HashMap<String, String>> submissions = new ArrayList<HashMap<String,String>>();
+	private GetLiveSubmissionsTask liveSubmissionTask = null;
+	private GetLatestLiveTask latestLiveTask = null;
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		mContentView = inflater.inflate(R.layout.live_submission_fragment,
+				container, false);
+		list = (ListView) mContentView.findViewById(R.id.list);
+//		list.setOnRefreshListener(new OnRefreshListener() {
+//			@Override
+//			public void onRefresh() {
+//				latestLiveTask = new GetLatestLiveTask();
+//				latestLiveTask.execute(CommonUtils.LIVE_SUBMISSION_URL
+//						+ sinceID);
+//			}
+//		});
+		return super.onCreateView(inflater, container, savedInstanceState);
+	}
+
+	@SuppressLint("NewApi")
+	@Override
+	public void onActivityCreated(Bundle savedInstanceState) {
+		super.onActivityCreated(savedInstanceState);
+		setContentView(mContentView);
+		setContentShown(false);
+		setEmptyText(R.string.empty);
+		ConnectionDetector cd = new ConnectionDetector(getActivity());
+		if (cd.isConnectingToInternet()) {
+			liveSubmissionTask = new GetLiveSubmissionsTask();
+			liveSubmissionTask.execute(CommonUtils.LIVE_SUBMISSION_URL
+						+ "0");
+		} else {
+			MainActivity.networkAvailabilityNotice(getActivity());
+		}
+	}
+
+	@Override
+	public void onPause() {
+		super.onPause();
+	}
+
+	protected class GetLiveSubmissionsTask extends
+			AsyncTask<String, Void, ArrayList<HashMap<String, String>>> {
+
+		@Override
+		protected ArrayList<HashMap<String, String>> doInBackground(String... params) {
+			InputStreamReader isr = new JSONDownloader().getJSONStringFromUrl(params[0]);
+			ArrayList<HashMap<String, String>> result = null;
+			if (isr != null) {
+				result = new ArrayList<HashMap<String, String>>();
+				MainActivity activity = (MainActivity) getActivity();
+				JsonReader reader = new JsonReader(isr);
+				try {
+					reader.beginArray();
+					while (reader.hasNext()) {
+						reader.beginObject();
+						while (reader.hasNext()) {
+							reader.nextName(); // id
+							reader.skipValue();
+							reader.nextName(); // type
+							if (reader.nextString().equals("lastsubs")) {
+								String message = reader.nextName(); // msg
+								if (message.equals(CommonUtils.KEY_MESSAGE)) {
+									reader.beginObject();
+									HashMap<String, String> map = new HashMap<String, String>();
+									reader.skipValue(); // sid
+									reader.skipValue();
+									reader.skipValue(); // uid
+									reader.skipValue();
+									String problemID = reader.nextName(); // pid
+									if (problemID
+											.equals(CommonUtils.KEY_PROBLEM_ID)) {
+										String txt = "Latest problem";
+										Problems obj = MainActivity.problems
+												.get(reader.nextInt());
+										if (obj != null) {
+											txt = obj.getProblemsInfo();
+										}
+										map.put(CommonUtils.KEY_PROBLEM_ID, txt);
+									}
+									String verdictID = reader.nextName(); // verdict
+									if (verdictID
+											.equals(CommonUtils.KEY_VERDICT_ID)) {
+										Verdict verdict = activity.verdicts
+												.get(reader.nextString());
+										map.put(CommonUtils.KEY_VERDICT_COLOR,
+												verdict.verdictColorHex);
+										map.put(CommonUtils.KEY_VERDICT_ID,
+												verdict.name);
+									}
+									String languageID = reader.nextName(); // lang
+									if (languageID
+											.equals(CommonUtils.KEY_LANGUAGE_ID)) {
+										map.put(CommonUtils.KEY_LANGUAGE_ID,
+												activity.languageCode
+														.get(reader
+																.nextString()));
+									}
+									String execTime = reader.nextName(); // run
+									if (execTime
+											.equals(CommonUtils.KEY_RUNTIME)) {
+										Double runTime = reader.nextDouble() / 1000;
+										map.put(CommonUtils.KEY_RUNTIME,
+												runTime.toString() + "s");
+									}
+									reader.skipValue(); // mem
+									reader.skipValue();
+									String rank = reader.nextName(); // rank
+									if (rank.equals(CommonUtils.KEY_SUBMISSION_RANK)) {
+										map.put(CommonUtils.KEY_SUBMISSION_RANK,
+												reader.nextString());
+									}
+									reader.skipValue(); // sbt
+									reader.skipValue();
+									if (reader.nextName().equals( // name
+											CommonUtils.KEY_NAME)) {
+										map.put(CommonUtils.KEY_NAME,
+												reader.nextString());
+									}
+									if (reader.nextName().equals( // uname
+											CommonUtils.KEY_USERNAME)) {
+										map.put(CommonUtils.KEY_USERNAME,
+												reader.nextString());
+									}
+									result.add(map);
+									reader.endObject();
+								}
+							}
+
+						}
+						reader.endObject();
+					}
+					reader.endArray();
+				} catch (IOException e) {
+					result = null;
+				} finally {
+					try {
+						reader.close();
+					} catch (IOException e) {
+						
+					}
+				}
+			}
+			if (result != null)
+				Collections.reverse(result);
+			return result;
+		}
+
+		@Override
+		protected void onPostExecute(ArrayList<HashMap<String, String>> result) {
+			if (result != null) {
+				submissions = result;
+				adapter = new LiveSubmissionAdapter(getActivity(),
+						submissions);
+				list.setAdapter(adapter);
+				setContentEmpty(false);
+				setContentShown(true);
+			} 
+		}
+	}
+
+	protected class GetLatestLiveTask extends
+			AsyncTask<String, Void, ArrayList<HashMap<String, String>>> {
+
+		@Override
+		protected ArrayList<HashMap<String, String>> doInBackground(String... params) {
+			InputStreamReader isr = new JSONDownloader().getJSONStringFromUrl(params[0]);
+			ArrayList<HashMap<String, String>> result = null;
+			if (isr != null) {
+				MainActivity activity = (MainActivity) getActivity();
+				JsonReader reader = new JsonReader(isr);
+				result = new ArrayList<HashMap<String, String>>();
+				try {
+					reader.beginArray();
+					while (reader.hasNext()) {
+						reader.beginObject();
+						while (reader.hasNext()) {
+							reader.nextName(); // id
+							reader.skipValue();
+							reader.nextName(); // type
+							if (reader.nextString().equals("lastsubs")) {
+								String message = reader.nextName(); // msg
+								if (message.equals(CommonUtils.KEY_MESSAGE)) {
+									reader.beginObject();
+									HashMap<String, String> map = new HashMap<String, String>();
+									reader.skipValue(); // sid
+									reader.skipValue();
+									reader.skipValue(); // uid
+									reader.skipValue();
+									String problemID = reader.nextName(); // pid
+									if (problemID
+											.equals(CommonUtils.KEY_PROBLEM_ID)) {
+										String txt = "Latest problem";
+										Problems obj = MainActivity.problems
+												.get(reader.nextInt());
+										if (obj != null) {
+											txt = obj.getProblemsInfo();
+										}
+										map.put(CommonUtils.KEY_PROBLEM_ID, txt);
+									}
+									String verdictID = reader.nextName(); // verdict
+									if (verdictID
+											.equals(CommonUtils.KEY_VERDICT_ID)) {
+										Verdict verdict = activity.verdicts
+												.get(reader.nextString());
+										map.put(CommonUtils.KEY_VERDICT_COLOR,
+												verdict.verdictColorHex);
+										map.put(CommonUtils.KEY_VERDICT_ID,
+												verdict.name);
+									}
+									String languageID = reader.nextName(); // lang
+									if (languageID
+											.equals(CommonUtils.KEY_LANGUAGE_ID)) {
+										map.put(CommonUtils.KEY_LANGUAGE_ID,
+												activity.languageCode
+														.get(reader
+																.nextString()));
+									}
+									String execTime = reader.nextName(); // run
+									if (execTime
+											.equals(CommonUtils.KEY_RUNTIME)) {
+										Double runTime = reader.nextDouble() / 1000;
+										map.put(CommonUtils.KEY_RUNTIME,
+												runTime.toString() + "s");
+									}
+									reader.skipValue(); // mem
+									reader.skipValue();
+									if (reader.nextName().equals(
+											CommonUtils.KEY_SUBMISSION_RANK)) { // rank
+										map.put(CommonUtils.KEY_SUBMISSION_RANK,
+												reader.nextString());
+									}
+									reader.skipValue(); // sbt
+									reader.skipValue();
+									if (reader.nextName().equals( // name
+											CommonUtils.KEY_NAME)) {
+										map.put(CommonUtils.KEY_NAME,
+												reader.nextString());
+									}
+									if (reader.nextName().equals( // uname
+											CommonUtils.KEY_USERNAME)) {
+										map.put(CommonUtils.KEY_USERNAME,
+												reader.nextString());
+									}
+									result.add(map);
+									reader.endObject();
+								}
+							}
+
+						}
+						reader.endObject();
+					}
+					reader.endArray();
+				} catch (IOException e1) {
+					result = null;
+				} finally {
+					try {
+						reader.close();
+					} catch (IOException e) {
+						
+					}
+				}
+			}
+			return result;
+		}
+
+		@Override
+		protected void onPostExecute(ArrayList<HashMap<String, String>> result) {
+			if (result != null) {
+				for (int i = 0; i < result.size(); i++) {
+					submissions.add(0, result.get(i));
+				}
+				//list.onRefreshComplete();
+			}
+		}
+
+	}
+
+	@Override
+	public void onStop() {
+		super.onStop();
+		if (liveSubmissionTask != null
+				&& liveSubmissionTask.getStatus() != AsyncTask.Status.FINISHED) {
+			liveSubmissionTask.cancel(true);
+		}
+		if (latestLiveTask != null
+				&& latestLiveTask.getStatus() != AsyncTask.Status.FINISHED) {
+			latestLiveTask.cancel(true);
+		}
+	}
+}
