aa867da6e6b5a742a503ea2613b2f1927ca13522
Esraa Ibrahim <Esraa@Esraa-Ibrahims-Mac-mini.local>
2020-01-11 23:19:17 +0200

    Problems view pager and connect it to tabs layout

--- uhunt/app/src/main/java/org/onlinejudge/uhunt/cpbook/CPBookFragment.kt
+++ uhunt/app/src/main/java/org/onlinejudge/uhunt/cpbook/CPBookFragment.kt
@@ -20,14 +20,19 @@
 
 package org.onlinejudge.uhunt.cpbook
 
-import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
+import kotlinx.android.synthetic.main.fragment_cpbook.*
 
 import org.onlinejudge.uhunt.R
 
@@ -39,15 +44,57 @@ class CPBookFragment : Fragment() {
 
     private lateinit var viewModel: CPBookViewModel
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(CPBookViewModel::class.java)
+    }
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_cpbook, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        tabsViewPager.adapter = ProblemsCategoryStateAdapter(this)
+
+        mainCategoriesTabLayout.addOnTabSelectedListener(object: TabLayout.OnTabSelectedListener {
+            override fun onTabSelected(tab: TabLayout.Tab) {
+                when(tab.position) {
+                    0 -> {
+
+                    }
+                    1 -> {
+
+                    }
+                    2 -> {
+
+                    }
+                }
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab) {
+                //TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab) {
+                //TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+        })
+
+        TabLayoutMediator(mainCategoriesTabLayout, tabsViewPager) { tab, position ->
+            tab.icon = when(position) {
+                0 -> ContextCompat.getDrawable(requireContext(), R.drawable.basics_icon)
+                1 -> ContextCompat.getDrawable(requireContext(), R.drawable.intermediate_icon)
+                else -> ContextCompat.getDrawable(requireContext(), R.drawable.advanced_icon)
+            }
+        }.attach()
+    }
+
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProviders.of(this).get(CPBookViewModel::class.java)
-        viewModel.getCPBookProblems().observe(this, Observer { problemCategoryList ->
+        viewModel.getCPBookProblems().observe(viewLifecycleOwner, Observer { problemCategoryList ->
             Log.d("Tesssssst", problemCategoryList[0].title)
         })
     }
